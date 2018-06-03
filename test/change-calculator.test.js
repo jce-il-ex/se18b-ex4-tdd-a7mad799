7c1ad3d2375e7f774ec5d222877e14c25d511642
a7mad799 <a7mad.1.2008@gmail.com>
2018-06-03 19:52:14 +0300

    RED: Change Calculator

---
+++ test/change-calculator.test.js
@@ -0,0 +1,4 @@
+var coins = [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
+
+var test = require('tape'); // assign the tape library to the variable "test"
+var calculateChange = require('../lib/change-calculator.js');  // require (not-yet-written) module
